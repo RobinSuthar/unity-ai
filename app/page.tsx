0d6e82476afb35e6aba98c8eb3b42ecd6af3c5ee
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-08-21 19:28:46 -0600

    More Inhancement to card swap

--- app/page.tsx
+++ app/page.tsx
@@ -9,27 +9,41 @@ export default function Home() {
   return (
     <div>
       <BackgroundRippleEffectDemo />
-      <div style={{ height: "600px", position: "relative" }}>
-        <CardSwap
-          cardDistance={60}
-          verticalDistance={70}
-          delay={5000}
-          pauseOnHover={false}
-        >
-          <Card>
-            <Image src={image} height={1000} width={600} />
-            <h3>Card 1</h3>
-            <p>Your content here</p>
-          </Card>
-          <Card>
-            <Image src={image} height={800} width={600} />
-          </Card>
-          <Card>
-            <Image src={image} height={800} width={600} />
-          </Card>
-        </CardSwap>
+      <div className="grid grid-cols-2">
+        <div>
+          <div className="mt-60 w-full">
+            <h2 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-neutral-800 md:text-4xl lg:text-7xl dark:text-neutral-100">
+              Interactive Background Boxes Ripple Effect
+            </h2>
+            <p className="relative z-10 mx-auto mt-4 max-w-xl text-center text-neutral-800 dark:text-neutral-500">
+              Hover over the boxes above and click.To be used on backgrounds of
+              hero sections OR Call to Action sections. I beg you don&apos;t use
+              it everywhere.
+            </p>
+          </div>
+        </div>
+
+        <div style={{ height: "600px", position: "relative" }}>
+          <CardSwap
+            cardDistance={60}
+            verticalDistance={70}
+            delay={5000}
+            pauseOnHover={false}
+          >
+            <Card>
+              <Image src={image} height={1000} width={600} />
+              <h3>Card 1</h3>
+              <p>Your content here</p>
+            </Card>
+            <Card>
+              <Image src={image} height={800} width={600} />
+            </Card>
+            <Card>
+              <Image src={image} height={800} width={600} />
+            </Card>
+          </CardSwap>
+        </div>
       </div>
-      ;
       <HeroScrollDemo />
     </div>
   );
