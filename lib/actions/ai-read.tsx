b20e044a9961fbdf3e5f317c07a6821d9c1066ec
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-09-16 11:54:20 -0600

    GPT-3 Taking added

--- lib/actions/ai-read.tsx
+++ lib/actions/ai-read.tsx
@@ -1,16 +1,12 @@
 "use server";
 import OpenAI from "openai";
-console.log(process.env.OPENAI_API_KE);
 
 export async function AIRead(userMessage: string) {
-  console.log(process.env.OPENAI_API_KE);
-
-  console.log("Asdas");
   const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const response = await client.responses.create({
-    model: "gpt-5",
+    model: "gpt-4",
     input: userMessage,
   });
 
