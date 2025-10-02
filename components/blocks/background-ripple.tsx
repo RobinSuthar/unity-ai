608816522d9dbb88c67db12da81f66e88d61645a
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-10-01 20:47:48 -0600

    Inhancement in UI dont

--- components/blocks/background-ripple.tsx
+++ components/blocks/background-ripple.tsx
@@ -12,7 +12,7 @@ export function BackgroundRippleEffectDemo() {
     <div className="relative flex min-h-screen p-18 w-full flex-col items-start justify-start overflow-hidden">
       <NavbarComponent />
       <BackgroundRippleEffect />
-      <div className="grid grid-cols-2 p-12 text-left">
+      <div className="grid grid-cols-2  text-left">
         <div className="mt-28 w-full">
           <h2 className="relative z-10 mx-auto max-w-4xl  text-2xl font-bold text-neutral-800 md:text-4xl lg:text-7xl dark:text-neutral-100">
             Worlds
@@ -35,7 +35,7 @@ export function BackgroundRippleEffectDemo() {
         </div>
         <div>
           {" "}
-          <div className=" relative z-10">
+          <div className=" relative z-10 mb-52">
             {" "}
             <SafariDemo />
           </div>
