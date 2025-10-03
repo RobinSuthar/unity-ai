0057007e8da439ab474b20ef7593179d24229b42
RobinSuthar <robinsuthar.rs07@gmail.com>
2025-10-02 23:59:28 -0600

    Minor Changes toFR

--- components/blocks/price-glow.tsx
+++ components/blocks/price-glow.tsx
@@ -67,14 +67,14 @@ const GridItem = ({ area, icon, title, description }: GridItemProps) => {
         />
         <div className="border-0.75 relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-xl p-6 md:p-6 dark:shadow-[0px_0px_27px_0px_#2D2D2D]">
           <div className="relative flex flex-1 flex-col justify-between gap-3">
+            <div className="w-fit rounded-lg border border-gray-600 p-2">
+              {icon}
+            </div>
             <div className="space-y-3">
-              <h3 className="-tracking-4 flex text-center gap-6 pt-0.5 font-sans text-xl/[1.375rem] font-semibold text-balance text-black md:text-2xl/[1.875rem] dark:text-white">
-                <div> {title}</div>
-                <div className="w-fit rounded-lg border border-gray-600 p-2">
-                  {icon}
-                </div>
+              <h3 className="-tracking-4 pt-0.5 font-sans text-xl/[1.375rem] font-semibold text-balance text-black md:text-2xl/[1.875rem] dark:text-white">
+                {title}
               </h3>
-              <h2 className="font-sans flex flex-row text-sm/[1.125rem] text-black md:text-base/[1.375rem] dark:text-neutral-400 [&_b]:md:font-semibold [&_strong]:md:font-semibold">
+              <h2 className="font-sans text-sm/[1.125rem] text-black md:text-base/[1.375rem] dark:text-neutral-400 [&_b]:md:font-semibold [&_strong]:md:font-semibold">
                 {description}
               </h2>
             </div>
